134f2726bea1aa7d151c82d75c24dd203dbb4c1f
Chandan Vempati <chanvem16@gmail.com>
2025-07-09 16:19:23 +0530

    Precompute subroutines results are as expected. Need to incorporate fplus, kernel and IFT subroutines

--- WH_solver_refactor/input_param_utils/input_param_utils.f90
+++ WH_solver_refactor/input_param_utils/input_param_utils.f90
@@ -14,25 +14,30 @@ module input_params
           integer                                    :: num_sup_zeros, num_sup_poles  !! number of supersonic zeros & poles
           integer                                    :: Nphi  !! polar mesh resolution for directivity computation
           complex(dpk), allocatable, dimension(:)    :: zeros_list_bw_s1_s2, poles_list_bw_s1_s2, sup_zeros_list, sup_poles_list
+          complex(dpk), allocatable, dimension(:)    :: kzsp
           real(dpk)                                  :: M1, M2, M3
           real(dpk)                                  :: h
           real(dpk)                                  :: kapT  !! sqrt(T1/T0)
           real(dpk)                                  :: kap_rho  !! rho1/rho0
           real(dpk)                                  :: theta,  w0, offset
           real(dpk)                                  :: Rmax, Rmin, Zmin, Zmax !! dimensions of the physical domain
-          real(dpk)                                  :: Zo  !! start point of incident vorticity (negative)
+          real(dpk), allocatable, dimension(:)       :: R, Z
           integer                                    :: Nmeshr, Nmeshz !! 400X400 -> Seg Fault!!
           real(dpk)                                  :: azim_mode
+          real(dpk)                                  :: Zo  !! start point of incident vorticity (negative)
           real(dpk)                                  :: vs_param_gamma  !! Vortex shedding parameter
           complex(dpk)                               :: KH_zero_1, KH_zero_2, KH_pole_1  !! instability zero 1
           complex(dpk)                               :: mu_plus !! axial wave no (of incident wave)
           complex(dpk)                               :: mu0 !! incident axial wave no (for incident vort)
+          complex(dpk)                               :: psi
           integer                                    :: num_IFT_pts_loop !! number of points for IFT contour
           real(dpk)                                  :: asymplim, asymplim1 !! asymptotic limit for the kernel functions
           real(dpk)                                  :: cnangle 
           real(dpk), allocatable, dimension(:)       :: cnanglei
           complex(dpk)                               :: omega_r
-  
+          complex(dpk)                               :: k_minus_at_mu_plus, k_plus_sz1,k_plus_sz2, kpsp1
+          real(dpk), allocatable, dimension(:)       :: phi  
+
           character(len=40) :: solution_mode
 
      end type input_params_t
--- WH_solver_refactor/user_defined_functions/user_defined_precompute.f90
+++ WH_solver_refactor/user_defined_functions/user_defined_precompute.f90
@@ -7,9 +7,7 @@ Module user_defined_precompute
  
   IMPLICIT NONE
   
-  PRIVATE :: 
   PUBLIC  :: precompute 
-
 
   CONTAINS 
  
@@ -22,16 +20,21 @@ Module user_defined_precompute
 !!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!=!!
 
     complex(dpk)          :: intgrl_A1_at_mu_plus, intgrl_A1_at_KH_zero_1, intgrl_A1_at_KH_zero_2, intgrl_at_sup_zero
-    complex(dpk)          :: k_plus_at_mu_plus, f1, f2, f3
+    complex(dpk)         :: k_plus_at_mu_plus, f1, f2, f3
+    complex(dpk)          :: alpha1, alpha2
     integer               :: f, i1
+    real(dpk)             :: PI
 
     type(input_params_t)   :: input_data
     type(contour_params_t) :: contour_data 
-  
+ 
+
+    PI = 4._dpk*ATAN(1.)
+ 
     if ((input_data%vortswitch == 1) .OR. (input_data%vortswitch == 2)) then
     !! the factor \Psi_{mn}(1) of (4.1) [see the JFM 2008]:
 
-       psi = 1
+      input_data%psi = 1
            !(input_data%omega_r)*resp*(1._dpk -((input_data%M2)*(input_data%mu_plus)))*compute_Trs_instab(1._dpk,input_data%mu_plus,2)* &
             !                   EXP(CMPLX(0._dpk,1._dpk,kind=dpk)*omega_r*mu_plus*(-Zo))
 
@@ -59,7 +62,7 @@ Module user_defined_precompute
 
        f1 = (1._dpk -( ( input_data%mu_plus)*(input_data%M1) ) /(1._dpk - (input_data%mu_plus)*(input_data%M2) )* &
                                                           bessj(alpha1*(input_data%h),input_data%azim_mode,1) &
-                                                                                  *EXP(ABS(AIMAG(alpha1* input_data%h)))
+                                                                                  *EXP(ABS(AIMAG(alpha1* input_data%h))))
       
        f2 = (bessj(alpha2,input_data%azim_mode,1)*dhank1(alpha2,input_data%azim_mode,1)- & 
          hank1(alpha2,input_data%azim_mode,1)*dbessj(alpha2,input_data%azim_mode,1))* &
@@ -70,14 +73,14 @@ Module user_defined_precompute
          hank1(alpha2*input_data%h,input_data%azim_mode,1)*dbessj(alpha2,input_data%azim_mode,1)* &
          EXP(CMPLX(0._dpk,1._dpk,kind=dpk)*alpha2*input_data%h + ABS(AIMAG(alpha2)))
 
-       psi = input_data%kap_rho*f1*f2/f3
+       input_data%psi = input_data%kap_rho*f1*f2/f3
        print*, 'precompute: Evaluated psi for the incident wave'
    
     end if
     
 !!  the factor Kt^{-}(\mu_{mn}^{+}):
 
-    if (vortswitch .EQ. 0) then
+    if (input_data%vortswitch .EQ. 0) then
      
         if (REAL(input_data%mu_plus) < contour_data%cont_cross_over_pt) then  !! mu_plus is below the contour; cont_cross_over_pt being the crossover pt
            print*, ''
@@ -89,12 +92,16 @@ Module user_defined_precompute
      end if
      
     print*, 'precompute: Evaluating kernel at mu_plus'
-    call compute_eqn_A1_integral(mu_plus,intgrl_A1_at_mu_plus,0,0,1)
+
+    call compute_eqn_A1_integral(input_data%mu_plus,intgrl_A1_at_mu_plus,0,0,1,input_data,contour_data)
        
     k_plus_at_mu_plus = EXP(-intgrl_A1_at_mu_plus/(2._dpk*PI*CMPLX(0._dpk,1._dpk,kind=dpk)) + & 
-                        LOG(compute_kernel(0,input_data%mu_plus)/compute_U_s_factor(0,input_data%mu_plus)))
+                        LOG(compute_kernel(0,input_data%mu_plus,input_data)/compute_U_s_factor(0,input_data%mu_plus,input_data)))
 
-    k_minus_at_mu_plus =  compute_kernel(0,input_data%mu_plus)/(k_plus_at_mu_plus*compute_U_s_factor(0,input_data%mu_plus)) 
+  !  write(*,'(/A12,2X,2F15.10)') ' integral at mu_plus:->', intgrl_A1_at_mu_plus
+ 
+    input_data%k_minus_at_mu_plus =  compute_kernel(0,input_data%mu_plus,input_data)/ &
+                          (k_plus_at_mu_plus*compute_U_s_factor(0,input_data%mu_plus,input_data)) 
 
 !!  the factor Kt^{+}(s_{z1}):
 
@@ -103,25 +110,33 @@ Module user_defined_precompute
        print*, 'precompute: Evaluating Kt^{+}(s_{z1}) at KH_zero_1'
        call compute_eqn_A1_integral(input_data%KH_zero_1,intgrl_A1_at_KH_zero_1,0,0,1,input_data,contour_data)
 
-       k_plus_sz1 = EXP(-intgrl_A1_at_KH_zero_1/(2._dpk*PI*CMPLX(0._dpk,1._dpk,kind=dpk))) 
+
+       input_data%k_plus_sz1 = EXP(-intgrl_A1_at_KH_zero_1/(2._dpk*PI*CMPLX(0._dpk,1._dpk,kind=dpk))) 
        !! NOTE: zero KH_zero_1 has to lie below  !! the contour
+
+   !    write(*,'(/A22,2X,2F20.10/)') 'Integral at KH zero 1:->', intgrl_A1_at_KH_zero_1
+
     end if
 !!  the factor K^{+}(s_{z2}):
 
     print*, 'precompute: Evaluating Kt^{+}(s_{z2}) at KH_zero_2'
     call compute_eqn_A1_integral(input_data%KH_zero_2,intgrl_A1_at_KH_zero_2,0,0,1,input_data,contour_data)
 
-    k_plus_sz2 = EXP(-intgrl_A1_at_KH_zero_2/(2._dpk*PI*CMPLX(0._dpk,1._dpk,kind=dpk)))  !! same for KH_zero_2
+    input_data%k_plus_sz2 = EXP(-intgrl_A1_at_KH_zero_2/(2._dpk*PI*CMPLX(0._dpk,1._dpk,kind=dpk)))  !! same for KH_zero_2
 
-    if (input_data%num_sup_zeros > 0) allocate(kzsp(input_data%num_sup_zeros))
+  !  write(*,'(/A22,2X,2F20.10/)') 'Integral at KH zero 2:->', intgrl_A1_at_KH_zero_2
+
+    if (input_data%num_sup_zeros > 0) allocate(input_data%kzsp(input_data%num_sup_zeros))
 
     if (input_data%vortswitch .EQ. 0) then
       do i1 = 1, input_data%num_sup_zeros  
        print '(A, I0)', 'precompute: Evaluating kernel at supersonic zero ', i1
-       call compute_eqn_A1_integral(sup_zeros_list(i1),intgrl_at_sup_zero,0,0,1,input_data,contour_data )
+       call compute_eqn_A1_integral(input_data%sup_zeros_list(i1),intgrl_at_sup_zero,0,0,1,input_data,contour_data)
   
-       kzsp(i1) = EXP(-intgrl_at_sup_zero/(2._dpk*PI*CMPLX(0._dpk,1._dpk,kind=dpk)))  !! NOTE: instability zeros are below
-  
+       input_data%kzsp(i1) = EXP(-intgrl_at_sup_zero/(2._dpk*PI*CMPLX(0._dpk,1._dpk,kind=dpk)))  !! NOTE: instability zeros are below
+
+       !write(*,'(/A12,2X,2F15.10)') ' k_plus_at_sup zero :->', intgrl_at_sup_zero
+
        end do
     end if
 
